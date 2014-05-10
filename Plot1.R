4770f9796d3a6aed0bab8823b9a67e8daaba763f
Muddsair <muddsairsharif@gmail.com>
2014-05-10 15:15:09 +0800

    Change the path of the dataset

--- Plot1.R
+++ Plot1.R
@@ -1,13 +1,13 @@
 #Plot1.script by Muddsair Sharif: muddsairsharif@gmail.com using RStudio 64Bit
 
 # Read the Dataset from CSv File
-HHPC <- read.csv("C:/DEP/HHPC.txt.txt", sep=";")
+HHPC <- read.csv("HHPC.txt", sep=";")
 
 #View the Dataset for verification
 View(HHPC)
 
 # Setting up the format of the PNG file heaight width etc as well as path
-png("C:/DEP/plot1.png", width = 480, height = 480)
+png("plot1.png", width = 480, height = 480)
 
 #Plot the Global Active Power i.e. Plot1
 hist(Global_active_power, main="Global Active Power", xlab="Global active power (in killowatts)", col="red")
