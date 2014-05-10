2649859da55170cc27c9e704c2cbf4129edb1723
Muddsair <muddsairsharif@gmail.com>
2014-05-10 15:12:39 +0800

    Create Plot1.R
    Just copy and paste code here.. after testing on local R Machine

---
+++ Plot1.R
@@ -0,0 +1,16 @@
+#Plot1.script by Muddsair Sharif: muddsairsharif@gmail.com using RStudio 64Bit
+
+# Read the Dataset from CSv File
+HHPC <- read.csv("C:/DEP/HHPC.txt.txt", sep=";")
+
+#View the Dataset for verification
+View(HHPC)
+
+# Setting up the format of the PNG file heaight width etc as well as path
+png("C:/DEP/plot1.png", width = 480, height = 480)
+
+#Plot the Global Active Power i.e. Plot1
+hist(Global_active_power, main="Global Active Power", xlab="Global active power (in killowatts)", col="red")
+
+# Switch off the grafic adapter after writing to file
+dev.off()
